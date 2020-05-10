a08acf7b2cf66133150b6b5ed14e019dce5666b8
guivmc <guivmc@hotmail.com>
2020-05-10 17:15:54 -0300

    Messages

---
+++ YourTravelTcc/Models/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace YourTravelTcc.Models
+{
+    public class Message
+    {
+        [Key]
+        public int ID { get; set; }
+        public String Text { get; set; }
+        public int SenderID { get; set; }
+        public int ReciverID { get; set; }
+    }
+}
