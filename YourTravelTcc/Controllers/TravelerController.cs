dafeae58d3b568ba0626678fdec038120267a80a
guivmc <guivmc@hotmail.com>
2020-05-12 16:43:02 -0300

    Documentation

--- YourTravelTcc/Controllers/TravelerController.cs
+++ YourTravelTcc/Controllers/TravelerController.cs
@@ -32,12 +32,18 @@ namespace YourTravelTcc.Controllers
             return View();
         }
 
+        /// <summary>
+        /// Search for guides in a given country.
+        /// </summary>
+        /// <param name="location">Country where to find the guides.</param>
+        /// <returns>A page with a list of guides.</returns>
         public IActionResult SearchForGuidesInLocation( String location )
         {
             //Get country that is being searched.
             CountryID countryID;
             Enum.TryParse( location, out countryID );
 
+            //Join guide with person table and search for country, return a enumerable of guides.
             var tableJoin = ( from guide in this._guideContext.Guide
                               join person in this._guideContext.Person on guide.ID equals person.ID
                               where person.CountryID.Equals( countryID )
